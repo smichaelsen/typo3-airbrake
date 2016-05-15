74f0015d53adba43cdeeec7154eb9e8910307567
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-05-15 08:32:36 +0200

    [FEATURE] Disable logging exceptions to file

--- Classes/ProductionExceptionHandler.php
+++ Classes/ProductionExceptionHandler.php
@@ -5,9 +5,21 @@ use Airbrake\EventHandler;
 use Smichaelsen\ShortcutParams\TypoScriptFrontendController;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\ContentObject\AbstractContentObject;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Lang\LanguageService;
 
 class ProductionExceptionHandler extends \TYPO3\CMS\Frontend\ContentObject\Exception\ProductionExceptionHandler
 {
+
+    /**
+     * @var array
+     */
+    protected $pluginConfiguration;
+
+    /**
+     * @var boolean
+     */
+    protected $stillLogExceptionToLogfile;
 
     /**
      * Handles exceptions thrown during rendering of content objects
@@ -22,21 +34,67 @@ class ProductionExceptionHandler extends \TYPO3\CMS\Frontend\ContentObject\Excep
      */
     public function handle(\Exception $exception, AbstractContentObject $contentObject = null, $contentObjectConfiguration = array())
     {
+        $pluginConfiguration = $this->getPluginConfiguration($contentObject->getContentObject());
+        $this->stillLogExceptionToLogfile = (bool)$pluginConfiguration['stillLogExceptionToLogfile'];
+        // Change the default message if no file logging is enabled. There is no need to output an exception identifier to the user.
+        if (!$this->stillLogExceptionToLogfile && !isset($this->configuration['errorMessage'])) {
+            $this->configuration['errorMessage'] = $this->getLanguageService()->sL('LLL:EXT:airbrake/Resources/Private/Language/locallang.xlf:defaultErrorMessage');
+        }
         $message = parent::handle($exception);
-        $pluginConfiguration = $this->getTyposcriptFrontendController()->tmpl->setup['plugin.']['tx_airbrake.'];
-        $apiKey = $contentObject->getContentObject()->stdWrap($pluginConfiguration['apiKey'], $pluginConfiguration['apiKey.']);
-        $host = $contentObject->getContentObject()->stdWrap($pluginConfiguration['host'], $pluginConfiguration['host.']);
-        $resource = $contentObject->getContentObject()->stdWrap($pluginConfiguration['resource'], $pluginConfiguration['resource.']);
-        $exceptionHandler = EventHandler::start($apiKey, false,
+        $exceptionHandler = EventHandler::start($pluginConfiguration['apiKey'], false,
             [
                 'secure' => true,
-                'host' => $host,
-                'resource' => $resource,
+                'host' => $pluginConfiguration['host'],
+                'resource' => $pluginConfiguration['resource'],
                 'environmentName' => (string)GeneralUtility::getApplicationContext(),
             ]
         );
         $exceptionHandler->onException($exception);
         return $message;
+    }
+
+    /**
+     * @param \Exception $exception
+     * @param string $errorMessage
+     * @param string $code
+     */
+    protected function logException(\Exception $exception, $errorMessage, $code)
+    {
+        if ($this->stillLogExceptionToLogfile) {
+            parent::logException($exception, $errorMessage, $code);
+        }
+    }
+
+    /**
+     * @param ContentObjectRenderer $cObj
+     * @return array
+     */
+    protected function getPluginConfiguration(ContentObjectRenderer $cObj)
+    {
+        static $pluginConfiguration;
+        if (!is_array($pluginConfiguration)) {
+            $pluginConfiguration = $this->getTyposcriptFrontendController()->tmpl->setup['plugin.']['tx_airbrake.'];
+            $stdWrapProperties = GeneralUtility::trimExplode(',', 'apiKey, host, resource, stillLogExceptionToLogfile');
+            foreach ($stdWrapProperties as $stdWrapProperty) {
+                if (empty($pluginConfiguration[$stdWrapProperty])) {
+                    $pluginConfiguration[$stdWrapProperty] = '';
+                }
+                if (empty($pluginConfiguration[$stdWrapProperty . '.'])) {
+                    $pluginConfiguration[$stdWrapProperty . '.'] = [];
+                }
+                $pluginConfiguration[$stdWrapProperty] = $cObj->stdWrap($pluginConfiguration[$stdWrapProperty], $pluginConfiguration[$stdWrapProperty . '.']);
+                unset($pluginConfiguration[$stdWrapProperty . '.']);
+            }
+        }
+        return $pluginConfiguration;
+    }
+
+    /**
+     * @return LanguageService
+     */
+    protected function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
     }
 
     /**
--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # TYPO3 Extension: Airbrake
 
-Logs PHP Exceptions to <a href="https://airbrake.io/">airbrake.io</a> or alternative services that are API compliant to airbrake.io
+Logs PHP Exceptions to <a href="https://airbrake.io/">airbrake.io</a> or alternative services that are API compliant to airbrake.io.
+At the moment only exceptions in ContentElement are handled. Other frontendexceptions and all backend exceptions are not supported (handled) yet.
 
-## Configuration
+## Quick Setup
 
-After installing the extension, include it's Static TypoScript Template and then configure your airbrake.io API key in the TS Constant Editor.
+After installing the extension, include its Static TypoScript Template and then configure your airbrake.io API key in the TS Constant Editor.
+
+## TypoScript Reference
+
+These options are available in `plugin.tx_airbrake`. All of them are also configurable via the constant editor. All of them have `stdWrap` enabled.
+
+| option | default | description |
+| ------ | ------- | ----------- |
+| apiKey | *(empty)* | API Key for airbrake |
+| host | `api.airbrake.io` | URL of your airbrake host. |
+| resource | `/notifier_api/v2/notices` | Resource to call to log exceptions. This depends on the host. You probably only need to change the resouce if you use an alternative airbrake host |
+| stillLogExceptionToLogfile | `false` | TYPO3 usually logs production exceptions to typo3temp/logs/ and airbrake disables this behaviour. If this is set to true, TYPO3 will keep logging to the file. |
+
+## Other TypoScript
+
+These settings are not introduced by `EXT:airbrake` but can be useful for you:
+
+| option | description |
+| ------ | ----------- |
+| config.contentObjectExceptionHandler | This is set to `Smichaelsen\Airbrake\ProductionExceptionHandler` by `EXT:airbrake` to register it as exception handler for content objects. Unsetting it will restore TYPO3's default behaviour. |
+| config.contentObjectExceptionHandler.errorMessage | This is TYPO3's option to change the default message ("Oops, an error occurred! Code: SOMEEXCEPTIONIDENTIFIER"). Also note that `EXT:airbrake` changes the TYPO3 default message to a localized message if you set nothing here. |
