9b1e9b5b5844676492d8a3649c2d2c64fa883a23
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-05-17 21:25:33 +0200

    [!!!][TASK] Switch to new official airbrake package

--- Classes/Service/AirbrakeService.php
+++ Classes/Service/AirbrakeService.php
@@ -1,28 +1,23 @@
 <?php
 namespace Smichaelsen\Airbrake\Service;
 
-use Airbrake\EventHandler;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class AirbrakeService implements SingletonInterface
 {
-    /**
-     * @param \Exception $exception
-     */
     public function handleException(\Exception $exception)
     {
         $pluginConfiguration = GeneralUtility::makeInstance(PluginConfigurationService::class)->getPluginConfiguration();
-        $exceptionHandler = EventHandler::start(
-            $pluginConfiguration['apiKey'],
-            false,
+        $notifier = new \Airbrake\Notifier(
             [
-                'secure' => true,
+                'projectId' => $pluginConfiguration['projectId'],
+                'projectKey' => $pluginConfiguration['projectKey'],
                 'host' => $pluginConfiguration['host'],
-                'resource' => $pluginConfiguration['resource'],
-                'environmentName' => (string)GeneralUtility::getApplicationContext(),
+                'environment' => (string)GeneralUtility::getApplicationContext(),
             ]
         );
-        $exceptionHandler->onException($exception);
+        \Airbrake\Instance::set($notifier);
+        \Airbrake\Instance::notify($exception);
     }
 }
